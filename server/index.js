198cc5ad4ac5fa96570c4438ba0fb1578d7b84d9
Patrick Orihuela <patrick.orihuela@gmail.com>
2022-08-01 10:19:58 -0600

    zz

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -19,5 +23,5 @@ mongoose
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
   .catch((error) => console.log(error.message));
-  
-//mongoose.set('useFindAndModify', false);
+
+// mongoose.set('useFindAndModify', false);
